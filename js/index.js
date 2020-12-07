9a6b28c71233c291bfd036aaeb4083d143364aea
Felipe Quintero <ffquint@gmail.com>
2020-12-06 19:27:28 -0500

    added code for mouseover event

--- js/index.js
+++ js/index.js
@@ -1,5 +1,28 @@
 // Your code goes here
 // * `mouseover`
+const imgTags = Array.from(document.querySelectorAll('img'));
+console.log(imgTags)
+
+
+// function imgBorder(e) {
+//     e.target.style.border = '5px solid green';
+// };
+
+
+// reset the color after a short delay
+// setTimeout(function() {
+//     event.target.style.color = "";
+//   }, 500);
+// }, false);
+
+document.addEventListener('mouseover', (e) => {
+    e.target.style.border = '2.5px solid green';
+
+    setTimeout(function() {
+        e.target.style.border = '';
+    }, 400);
+}, false);
+
 
 
 // * `keydown`
